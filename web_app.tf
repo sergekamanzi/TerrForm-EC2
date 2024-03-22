6b5700fe663719aad84ccaf88e0dc82bfce17310
serge.kmz <127386891+sergekamanzi@users.noreply.github.com>
2024-03-22 16:44:12 +0200

    terraform-inf

---
+++ web_app.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "web-server" {
+  ami                    = "ami-0b8b44ec9a8f90422"
+  instance_type          = "t3.micro"
+  key_name               = "web-server-bastion"
+  subnet_id              = aws_subnet.web-private-a.id
+  vpc_security_group_ids = [aws_security_group.web-app-security-group.id]
+  iam_instance_profile   = aws_iam_instance_profile.server_profile.name
+
+  tags = {
+    Name = "web-server"
+  }
+
+  root_block_device {
+    delete_on_termination = false
+    encrypted             = true
+    volume_size           = 20
+    volume_type           = "gp3"
+  }
+
+}
+
+resource "aws_instance" "app-server" {
+  ami                    = "ami-0b8b44ec9a8f90422"
+  instance_type          = "t3.micro"
+  key_name               = "web-server-bastion"
+  subnet_id              = aws_subnet.app-private-a.id
+  vpc_security_group_ids = [aws_security_group.web-app-security-group.id]
+  iam_instance_profile   = aws_iam_instance_profile.server_profile.name
+
+  tags = {
+    Name = "app-server"
+  }
+
+  root_block_device {
+    delete_on_termination = false
+    encrypted             = true
+    volume_size           = 20
+    volume_type           = "gp3"
+  }
+
+}
